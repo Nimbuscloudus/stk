c1b2f775301a6c5e5a4dcd5c58634c3ee76f9bb6
Nimbuscloudus <90365524+Nimbuscloudus@users.noreply.github.com>
2024-03-30 20:28:27 +0800

    Update home.py

--- home.py
+++ home.py
@@ -40,6 +40,7 @@ if st.button('Тооцоолох'):
     st.write("Тохирох жин:")
     for company, weight in zip(stocks.columns, optimal_weights.x):
         st.write(f"{company}: {round(budget*weight, 2)}")
-# Display time series data
-st.write("Stocks' Time Series Data:")
-st.line_chart(stocks)
+st.write("Түүх:")
+for company in stocks.columns:
+    st.write(f"### {company}")
+    st.line_chart(stocks[company])
